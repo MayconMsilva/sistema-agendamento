71d88a28a4b9fdce25f3d6016e6fa1585df95dfd
JAVA-DIO <mayconmsilva01@gmail.com>
2026-07-22 18:35:54 -0300

    feat: Criado Arquivo Controller Auth

---
+++ src/main/java/com/sistema_empresa/demo/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.sistema_empresa.demo.controller;
+
+import com.sistema_empresa.demo.dto.request.LoginRequest;
+import com.sistema_empresa.demo.dto.request.RegisterRequest;
+import com.sistema_empresa.demo.dto.response.AuthResponse;
+import com.sistema_empresa.demo.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
