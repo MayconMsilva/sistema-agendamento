d8cec067b1d89d885a59690b0d1868c48cffe850
JAVA-DIO <mayconmsilva01@gmail.com>
2026-07-22 17:45:02 -0300

    feat: Criado arquivos enums

---
+++ src/main/java/com/sistema_empresa/demo/domain/enums/Role.java
@@ -0,0 +1,7 @@
+package com.sistema_empresa.demo.domain.enums;
+
+public enum Role {
+    ADMIN,
+    PROFISSIONAL,
+    CLIENTE
+}
---
+++ src/main/java/com/sistema_empresa/demo/domain/enums/StatusAgendamento.java
@@ -0,0 +1,8 @@
+package com.sistema_empresa.demo.domain.enums;
+
+public enum StatusAgendamento {
+    PENDENTE,
+    CONFIRMADO,
+    CANCELADO,
+    CONCLUIDO
+}
