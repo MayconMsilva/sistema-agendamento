da74e49dd3648fbb501ed4a6403722ff7883a818
JAVA-DIO <mayconmsilva01@gmail.com>
2026-07-22 18:02:29 -0300

    feat: Criado Arquivo Exception

---
+++ src/main/java/com/sistema_empresa/demo/exception/ConflitoHorarioException.java
@@ -0,0 +1,7 @@
+package com.sistema_empresa.demo.exception;
+
+public class ConflitoHorarioException extends RuntimeException {
+    public ConflitoHorarioException(String message) {
+        super(message);
+    }
+}
